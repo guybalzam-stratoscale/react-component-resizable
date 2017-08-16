b08c4ebd6b9ebb186520da695cdafb582ec42387
Guy Balzam <guybal@gmail.com>
2017-08-16 10:54:38 +0300

    upgrade to support react-16

--- src/component.js
+++ src/component.js
@@ -1,67 +1,53 @@
 var React = global.React || require('react');
+var PropTypes = require('prop-types')
 var objectAssign = require('object-assign');
 
-var Resizable = React.createClass({
+class Resizable extends React.components {
+  constructor(props) {
+    super(props)
+    this.lastDimensions = {
+      width: null,
+      height: null
+    }
+  }
 
-  lastDimensions: {
-    width: null,
-    height: null
-  },
+  requestFrame(fn) {
+    return (window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function(fn) { return window.setTimeout(fn, 20); })(fn);
+  }
 
-  propTypes: {
-    triggersClass: React.PropTypes.string,
-    expandClass  : React.PropTypes.string,
-    contractClass: React.PropTypes.string,
-    embedCss     : React.PropTypes.bool,
-    onResize     : React.PropTypes.func.isRequired
-  },
-
-  getDefaultProps: function () {
-    return {
-      triggersClass: 'resize-triggers',
-      expandClass: 'expand-trigger',
-      contractClass: 'contract-trigger',
-      embedCss: true
-    };
-  },
-
-  requestFrame: function (fn) {
-    return (window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function(fn){ return window.setTimeout(fn, 20); })(fn);
-  },
-
-  cancelFrame: function (id) {
+  cancelFrame(id) {
     return (window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.clearTimeout)(id);
-  },
+  }
 
-  componentDidMount: function () {
+  componentDidMount() {
     this.resetTriggers();
     this.initialResetTriggersTimeout = setTimeout(this.resetTriggers, 1000);
-  },
+  }
 
-  componentWillUnmount: function () {
+  componentWillUnmount() {
     clearTimeout(this.initialResetTriggersTimeout);
-  },
+  }
 
-  componentDidUpdate: function () {
+  componentDidUpdate() {
     this.resetTriggers();
-  },
+  }
 
-  resetTriggers: function () {
+  resetTriggers() {
     var contract = this.refs.contract;
     var expandChild = this.refs.expandChild;
     var expand = this.refs.expand;
 
-    contract.scrollLeft      = contract.scrollWidth;
-    contract.scrollTop       = contract.scrollHeight;
-    expandChild.style.width  = expand.offsetWidth + 1 + 'px';
+    contract.scrollLeft = contract.scrollWidth;
+    contract.scrollTop = contract.scrollHeight;
+    expandChild.style.width = expand.offsetWidth + 1 + 'px';
     expandChild.style.height = expand.offsetHeight + 1 + 'px';
-    expand.scrollLeft        = expand.scrollWidth;
-    expand.scrollTop         = expand.scrollHeight;
-  },
+    expand.scrollLeft = expand.scrollWidth;
+    expand.scrollTop = expand.scrollHeight;
+  }
 
-  onScroll: function () {
+  onScroll() {
     if (this.r) this.cancelFrame(this.r);
-    this.r = this.requestFrame(function () {
+    this.r = this.requestFrame(function() {
       var dimensions = this.getDimensions();
 
       if (this.haveDimensionsChanged(dimensions)) {
@@ -69,40 +55,51 @@ var Resizable = React.createClass({
         this.props.onResize(dimensions);
       }
     }.bind(this));
-  },
+  }
 
-  getDimensions: function () {
+  getDimensions() {
     var el = this.refs.resizable;
     return {
       width: el.offsetWidth,
       height: el.offsetHeight
     };
-  },
+  }
 
-  haveDimensionsChanged: function (dimensions) {
+  haveDimensionsChanged(dimensions) {
     return dimensions.width != this.lastDimensions.width || dimensions.height != this.lastDimensions.height;
-  },
+  }
 
-  render: function() {
-    const {triggersClass, expandClass, contractClass, embedCss, onResize, ...rest} = this.props;
-    var props = objectAssign({}, rest, {onScroll: this.onScroll, ref: 'resizable'});
+  render() {
+    const { triggersClass, expandClass, contractClass, embedCss, onResize, ...rest } = this.props;
+    var props = objectAssign({}, rest, { onScroll: this.onScroll, ref: 'resizable' });
     return (
-      React.createElement('div', props,
-        [
-          this.props.children,
-          React.createElement('div', {className: triggersClass, key: 'trigger'},
-            [
-              React.createElement('div', {className: expandClass, ref: 'expand', key: 'expand'}, React.createElement('div', {ref: 'expandChild'})),
-              React.createElement('div', {className: contractClass, ref: 'contract', key: 'contract'})
-            ]
-          ),
-          embedCss ? React.createElement('style', {key: 'embededCss', dangerouslySetInnerHTML: {__html: '.resize-triggers { visibility: hidden; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: \" \"; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }'}}) : null
-        ]
-      )
+      React.createElement('div', props, [
+        this.props.children,
+        React.createElement('div', { className: triggersClass, key: 'trigger' }, [
+          React.createElement('div', { className: expandClass, ref: 'expand', key: 'expand' }, React.createElement('div', { ref: 'expandChild' })),
+          React.createElement('div', { className: contractClass, ref: 'contract', key: 'contract' })
+        ]),
+        embedCss ? React.createElement('style', { key: 'embededCss', dangerouslySetInnerHTML: { __html: '.resize-triggers { visibility: hidden; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: \" \"; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }' } }) : null
+      ])
     );
   }
 
-});
+}
+
+Resizeable.propTypes = {
+  triggersClass: PropTypes.string,
+  expandClass: PropTypes.string,
+  contractClass: PropTypes.string,
+  embedCss: PropTypes.bool,
+  onResize: PropTypes.func.isRequired
+}
+
+Resizable.defaultProps = {
+  triggersClass: 'resize-triggers',
+  expandClass: 'expand-trigger',
+  contractClass: 'contract-trigger',
+  embedCss: true
+}
 
 module.exports = Resizable;
 global.Resizable = Resizable;
